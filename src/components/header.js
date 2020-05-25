74bbde880961909a18d1b8a363f324a374196d2e
Hernan Camilo Gomez Ruiz <gome109070@siigo.com>
2020-05-25 12:33:30 -0500

    Netlify Identity Widget

--- src/components/header.js
+++ src/components/header.js
@@ -4,75 +4,84 @@ import React from "react"
 
 import gatsbyLogo from '../images/gatsby-icon.png'
 
+import netlifyIdentity from 'netlify-identity-widget'
+
 const isActive = ({ isCurrent }) => {
   return { className: isCurrent ? 'active' : 'navlink' }
 }
 
 const NavLink = props => <Link getProps={isActive} {...props} />
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+class Header extends React.Component {
+  componentDidMount() {
+    netlifyIdentity.init()
+  }
+  render() {
+  const { siteTitle } = this.props
+  return (
+    <header
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-    {/* Title / Logo */}
-    <span style={{ display: 'flex', alignItems: 'center' }}>
-    <img  
-          src={gatsbyLogo} 
-          alt="Gatsby Garb Logo" 
-          style={{
-                borderRadius: '50%',
-                border: '3px solid orange',
-                margin: '0 5px',
-                width: '50px',
-              }} 
-              />
-      <h1 style={{ margin: 0 }}>
-        <NavLink to="/">{siteTitle}</NavLink>
-      </h1>
-    </span>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+      {/* Title / Logo */}
+      <span style={{ display: 'flex', alignItems: 'center' }}>
+      <img  
+            src={gatsbyLogo} 
+            alt="Gatsby Garb Logo" 
+            style={{
+                  borderRadius: '50%',
+                  border: '3px solid orange',
+                  margin: '0 5px',
+                  width: '50px',
+                }} 
+                />
+        <h1 style={{ margin: 0 }}>
+          <NavLink to="/">{siteTitle}</NavLink>
+        </h1>
+      </span>
 
-    <NavLink to="/blog">Blog</NavLink>
-    <NavLink to="/products">Store</NavLink>
+      <NavLink to="/blog">Blog</NavLink>
+      <NavLink to="/products">Store</NavLink>
+      <div data-netlify-identity-menu />
 
-    {/* Shopping cart summary */}
-    <div
-      style={{ color: 'white', cursor: 'pointer' }}
-      className="snipcart-summary snipcart-checkout"
-    >
-      <div>
-        <strong>My Cart</strong>
+      {/* Shopping cart summary */}
+      <div
+        style={{ color: 'white', cursor: 'pointer' }}
+        className="snipcart-summary snipcart-checkout"
+      >
+        <div>
+          <strong>My Cart</strong>
+        </div>
+        <div>
+          <span
+            style={{ fontWeight: 'bold' }}
+            className="snipcart-total-items"
+          />{' '}
+          Items in Cart
+        </div>
+        <div>
+          Total price{' '}
+          <span
+            style={{ fontWeight: 'bold' }}
+            className="snipcart-total-price"
+          />
+        </div>
+      </div>          
       </div>
-      <div>
-        <span
-          style={{ fontWeight: 'bold' }}
-          className="snipcart-total-items"
-        />{' '}
-        Items in Cart
-      </div>
-      <div>
-        Total price{' '}
-        <span
-          style={{ fontWeight: 'bold' }}
-          className="snipcart-total-price"
-        />
-      </div>
-    </div>          
-    </div>
-  </header>
-)
+    </header>
+)}}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
